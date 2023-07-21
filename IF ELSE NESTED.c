9cd11bcff951a5d2841b630c7cf4fab9d5ad667a
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-07-21 12:50:56 +0600

    Add files via upload

---
+++ IF ELSE NESTED.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int a=57;
+
+if(a>=75){
+printf ("GRADE A");}
+else if (a>=55 ){
+    if(a<75)
+{
+printf("Grade B");
+}
+}
+else{
+printf("Grade F");}
+
+return 0;
+}
